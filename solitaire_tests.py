3552d6e16eddd13f9bb667c8e55db1e420919004
Aaron Buckles <aaronjbuckles@outlook.com>
2017-08-18 12:19:21 -0700

    Added pile_sizes test

--- solitaire_tests.py
+++ solitaire_tests.py
@@ -14,6 +14,18 @@ class SolitaireTests(unittest.TestCase):
         self.deck.shuffle_cards()
         self.assertEqual(len(self.deck.cards), 52)
 
+    def test_pile_sizes(self):
+        self.deck.load_piles()
+        self.assertEqual(len(self.deck.piles[0].cards), 1)
+        self.assertEqual(len(self.deck.piles[1].cards), 2)
+        self.assertEqual(len(self.deck.piles[2].cards), 3)
+        self.assertEqual(len(self.deck.piles[3].cards), 4)
+        self.assertEqual(len(self.deck.piles[4].cards), 5)
+        self.assertEqual(len(self.deck.piles[5].cards), 6)
+        self.assertEqual(len(self.deck.piles[6].cards), 7)
+
+        self.assertEqual(len(self.deck.piles[7].cards), 24)
+
 
 if __name__ == '__main__':
     unittest.main()
